c0d2887368191b022158cd3cf839dc335edd1342
GizemTuncer7 <97919652+GizemTuncer7@users.noreply.github.com>
2022-02-04 17:32:30 +0300

    TileVania

---
+++ TileVania/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D myRigidBody;
+    [SerializeField] float moveSpeed=1f;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    private void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-Mathf.Sign(myRigidBody.velocity.x), 1f);
+    }
+}
